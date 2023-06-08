869da502b962b4ce0826f4b5a2a6a4eb74d3f6e6
RamalMammadzada <7kbac95@code.edu.az>
2023-06-08 01:31:49 -0700

    footer

--- myProject/assets/js/index.js
+++ myProject/assets/js/index.js
@@ -1,40 +1,46 @@
-const bar=document.getElementById("bar");
-const responsive=document.getElementById("responsive");
-const closeBtn=document.getElementById("closeBtn")
-bar.addEventListener("click",function(){
+const bar = document.getElementById("bar");
+const responsive = document.getElementById("responsive");
+const closeBtn = document.getElementById("closeBtn")
+bar.addEventListener("click", function () {
     responsive.classList.toggle("addResp");
 })
-closeBtn.addEventListener("click",function(){
+closeBtn.addEventListener("click", function () {
     responsive.classList.remove("addResp")
 })
 
-const cards=document.getElementById("cards");
+const cards = document.getElementById("cards");
 
 fetch("http://localhost:3000/write")
-.then((resp)=>resp.json())
-.then(data=>{
-    data.forEach((element,i)=>{
-        if(i<3){
-        let card1=document.createElement("div");
-        let p1=document.createElement("p");
-        let p2=document.createElement("p");
-        let flxdiv=document.createElement("div");
+    .then((resp) => resp.json())
+    .then(data => {
+        data.forEach((element, i) => {
+            if (i < 3) {
+                let card1 = document.createElement("div");
+                let p1 = document.createElement("p");
+                let p2 = document.createElement("p");
+                let flxdiv = document.createElement("div");
+                // let img = document.createElement("img");
+             
 
-        flxdiv.append(p1,p2);
-        card1.append(flxdiv);
-        cards.append(card1);
-        card1.classList.add("box1");
-        flxdiv.classList.add("flxdiv");
-        p2.classList.add("bigP");
-        p1.classList.add("littleP");
+                card1.classList.add("box1", "animate__animated","animate__backInUp");
+                flxdiv.classList.add("flxdiv");
+                p2.classList.add("bigP");
+                p1.classList.add("littleP");
 
 
-        p1.innerHTML=  element.money;
-        p2.innerHTML=  element.room;
-        // img.src=element.img;
-        //  card1.style.backgroundImage='url("element.img")';
 
-       
-        }
+                p1.innerHTML = element.money;
+                p2.innerHTML = element.room;
+
+                // img.src=element.img;
+
+                // card1.style.backgroundImage =`url("${element.img}")`;
+
+                flxdiv.append(p1, p2);
+                card1.append(flxdiv);
+                cards.append(card1);
+
+
+            }
+        })
     })
-})
